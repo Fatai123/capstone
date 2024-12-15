59bb8f6410d7cfbbc26ec8483ff0fd8c7ca67af3
ola-oye <olasunkanmioyetunji4@gmail.com>
2024-12-15 03:06:44 +0100

    add new ui feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from './components/Header';
 import Likes from './components/Likes';
+import Comments from './components/Comments';
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Likes />
-    </div>
+      <Comments />
   );
 }
 
---
+++ src/components/Comments.jsx
@@ -0,0 +1,10 @@
+function Comments() {
+    return (
+      <div className="comments">
+       <h1>Comment</h1>
+       <p>The boy is ..cidui</p>
+      </div>
+    );
+  }
+  
+  export default Comments;
